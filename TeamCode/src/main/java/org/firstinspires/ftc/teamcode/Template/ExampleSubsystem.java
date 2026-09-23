8af076db202a7e9ccdde85e83470ac70bde04cbb
Omar Abdelhamid <omar.ehab.us@gmail.com>
2026-09-22 22:05:36 -0700

    Examples and Templates

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Template/ExampleSubsystem.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.Template;
+
+
+import com.seattlesolvers.solverslib.command.SubsystemBase;
+
+public class ExampleSubsystem extends SubsystemBase {
+    /**
+     * Creates a new ExampleSubsystem.
+     */
+    public ExampleSubsystem() {
+
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+}
